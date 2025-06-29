3d668601a9e91cfbaf9020b66dc97d83bad69466
Kyryll Radchenko <radch3011@gmail.com>
2025-06-30 01:07:03 +0300

    implement top bar for screens (home, cart and wishlist)

--- app/src/main/java/com/example/e_commerceapp/ui/widgets/TopBar.kt
+++ app/src/main/java/com/example/e_commerceapp/ui/widgets/TopBar.kt
@@ -3,13 +3,20 @@ package com.example.e_commerceapp.ui.widgets
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.e_commerceapp.ui.screens.shoplist.IconTextButton
 
 class TopBar {
     companion object {
@@ -32,6 +39,20 @@ class TopBar {
                         contentDescription = "to Favourite"
                     )
                 }
+            }
+        }
+
+        @Composable
+        fun Main(
+            title : String,
+            onLogOut : () -> Unit
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth().padding(top = 10.dp).padding(horizontal = 15.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(title, fontSize = 32.sp)
+                IconTextButton(icon = Icons.AutoMirrored.Filled.Logout, backgroundColor = Color(0xFFffe7b5), text = "Log out", onClick = onLogOut)
             }
         }
     }
